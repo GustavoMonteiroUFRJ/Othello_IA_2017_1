dfab5252304279aad0e50c804daf665694d93f39
Gustavo <gusrmonteiro@live.com>
2017-07-01 01:22:17 -0300

    Nova estrutura para analizar melhor as jogadas
    Essa estrutura tem variáveis que podem ser usadas para tomada de decisão
    com diveros elementos. Agora pode-se olhar diversas características do
    movimento e decidir pesando diferentemente cada uma.

---
+++ TabuleiroOthello-master/models/moveQuality.py
@@ -0,0 +1,65 @@
+from models.move import Move
+
+class MoveQuality:
+  import random
+
+  def __init__(self, move, color):
+    self.move = move
+    self.color = color
+    self.score_gain = 0
+    self.is_corne = False
+    self.can_lose_corne = 0
+    self.power_to_gain_corne = 0
+    self.is_in_small_square = False
+    self.is_on_edgs = False
+
+  def analyze(self, board):
+
+    self.getCorner()
+    self.getPointGain(borad)
+    self.getMoveCornerLoser(board)
+    self.getLocation()
+
+  # seta vaiaveis de localisacao do movimento
+  def getLocation(self): 
+    if 2 < self.move.x < 7 and 2 < self.move.y < 7:
+      self.is_in_small_square = True
+    elif self.move.x == 1 or self.move.x == 8: 
+      if self.move.y == 1 or self.move.y == 8:
+        is_on_edgs = True
+
+  # seta vairavis relacionadas a quina
+  def getMoveCornerLoser(self,board):
+    corners = [[1,1],[1,8], [8,1], [8,8]]
+
+    borad.play(self.move,self.color)
+    opponente_moves = borad.valid_moves(board._opponent(self.color))
+
+    for corner in corners:
+      if Move(corner[0],corner[1]) in opponente_moves:
+        self.can_lose_corne += 1
+
+    for opponent_move in opponente_moves:
+      temp_borad = board.get_clone()
+      temp_borad.play(opponent_move,borad._opponent(self.color))
+      for corner in corners:
+      if Move(corner[0],corner[1]) in temp_borad.valid_moves(self.color):
+        self.power_to_gain_corne += 1
+    
+  # seta a quantidade de ponto ganho ao jogar
+  def getPointGain(self,board):
+    idx = 0
+    if self.color is board.BLACK:
+      idx = 1
+
+    temp_borad = board.get_clone()
+    temp_borad.play(self.move,self.color)
+    self.score_gain = temp_borad.score()[idx] - borad.score()[idx]
+  
+  # checa se é uma quina
+  def getCorner(self):
+    corners = [[1,1],[1,8], [8,1], [8,8]]
+    for corner in corners:
+      if Move(corner[0],corner[1]) == self.move:
+        self.is_corne = True
+        return

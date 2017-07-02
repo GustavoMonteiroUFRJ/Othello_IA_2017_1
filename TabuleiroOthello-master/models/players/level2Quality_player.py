553dee445e7dc99f45d68903c73d89778fefa66d
Gustavo <gusrmonteiro@live.com>
2017-07-02 19:37:36 -0300

    level2Quality  está jogando. Função nova ainda não testada

--- TabuleiroOthello-master/models/players/level2Quality_player.py
+++ TabuleiroOthello-master/models/players/level2Quality_player.py
@@ -1,10 +1,8 @@
 from models.move import Move
 from models.moveQuality import MoveQuality
-from operator import itemgetter, attrgetter
 
-
-class level2Player:
-  import random
+class Level2QualityPlayer:
+  
   # cada jogador tem geralmente 30 jogadas para fazer
   # quando faltar 9 jogadas eu digo eque o jogo esta acabando
   ENDING = 21
@@ -33,27 +31,25 @@ class level2Player:
       if move.is_corne:
         self.possible_moves += [move]
     if len(self.possible_moves) > 0:
-      print 'Achou uma quina...' #DEBUG
-      sorted(self.possible_moves, key=attrgetter('score_gain'))
-      print 'joguei na quina' #DEBUG
+      sorted(self.possible_moves, key=self.attrgetter('score_gain'))
       return self.possible_moves[0].move
     
     
     elif self.rounds_counter >= self.ENDING:
       for move in self.listMoveQuality:
         if move.can_lose_corne == 0:
-          self.possible_moves += move
-      sorted(self.possible_moves, key=attrgetter('score_gain'))
+          self.possible_moves += [move]
+      sorted(self.possible_moves, key=self.attrgetter('score_gain'))
 
     else:
       for move in self.listMoveQuality:
         if move.can_lose_corne == 0:
           self.possible_moves += [move]
-      sorted(self.possible_moves, key=attrgetter('score_gain'), reverse=True)
+      sorted(self.possible_moves, key=self.attrgetter('score_gain'), reverse=True)
 
 
     if len(self.possible_moves) == 0: # caso em que so tem lugares que se perde quina 
-      sorted(self.listMoveQuality , cmp=comper_movos_who_lose_corne) # ordena por pontos
+      sorted(self.listMoveQuality , cmp=self.comper_movos_who_lose_corne) # ordena por pontos
       return self.listMoveQuality[0].move # escolha a que da mais pontos
 
     else:
@@ -61,7 +57,7 @@ class level2Player:
 
   
   ## funcao que ordena crescenta para o can_lose_corne e decrescente para score_gain 
-  def comper_movos_who_lose_corne(m1,m2):
+  def comper_movos_who_lose_corne(self,m1,m2):
     if m1.can_lose_corne == m2.can_lose_corne:
       return m2.score_gain - m1.score_gain
     return m1.can_lose_corne - m2.can_lose_corne
@@ -95,8 +91,10 @@ class level2Player:
       array += [(move,maximize_score(temp_board.get_clone(), not(amIMax))[1] ) ]
     
     if amIMax:
-      array.sort(key=itemgetter(1), reverse=True) #Decrescente
+      array.sort(key=self.itemgetter(1), reverse=True) #Decrescente
     else:
-      array.sort(key=itemgetter(1)) #Crescente
+      array.sort(key=self.itemgetter(1)) #Crescente
 
     return array[0]
+
+  from operator import itemgetter, attrgetter

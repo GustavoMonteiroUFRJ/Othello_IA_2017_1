49841e4e5415c15856e9b88e642198207a2735e3
Alex <alexhandel96@googlemail.com>
2017-07-02 23:12:12 -0300

    limpeza do codigo

--- TabuleiroOthello-master/models/players/level2Quality_player.py
+++ TabuleiroOthello-master/models/players/level2Quality_player.py
@@ -1,10 +1,8 @@
 from models.move import Move
-# from models.moveQuality import MoveQuality
 
 class Level2QualityPlayer:
   
-  # cada jogador tem geralmente 30 jogadas para fazer
-  # quando faltar 9 jogadas eu digo eque o jogo esta acabando
+  #Definições de meio e fim do jogo
   MIDDLE=15
   ENDING=25
   ULTRAENDING=29
@@ -18,6 +16,7 @@ class Level2QualityPlayer:
 
     self.rounds_counter += 1
 
+    #do meio até o final do jogo, faz jogadas baseadas em depth search
     if self.rounds_counter >= self.MIDDLE:
       return self.maximize_score(board.get_clone(), root=True,depth=3)[0]
     elif self.rounds_counter >= self.ENDING:
@@ -34,13 +33,12 @@ class Level2QualityPlayer:
     # Aplicando a analize
     for move in self.listMoveQuality:
       move.analyze(board)
-      #print move #DEBUG
 
 
     # procurando por quinas e, caso exista, jogar na que gera mais pontos
     self.possible_moves = []
     for move in self.listMoveQuality:
-      if move.is_corne:
+      if move.is_corner:
         self.possible_moves += [move]
     if len(self.possible_moves) > 0:
       sorted(self.possible_moves, key=self.attrgetter('score_gain'))
@@ -48,36 +46,33 @@ class Level2QualityPlayer:
 
     else:
       for move in self.listMoveQuality:
-        if move.can_lose_corne == 0:
+        if move.can_lose_corner == 0:
           self.possible_moves += [move]
       sorted(self.possible_moves, key = self.attrgetter('score_gain'), reverse=True)
 
 
     if len(self.possible_moves) == 0: # caso em que so tem lugares que se perde quina 
-      sorted(self.listMoveQuality , cmp = self.comper_movos_who_lose_corne) # ordena por pontos
+      sorted(self.listMoveQuality , cmp = self.comper_movos_who_lose_corner) # ordena por pontos
       return self.listMoveQuality[0].move # escolha a que da mais pontos
 
     else:
       return self.possible_moves[0].move
 
   
-  ## funcao que ordena conforme crescente para o can_lose_corne e decrescente para score_gain 
-  def comper_movos_who_lose_corne(self,m1,m2):
-    if m1.can_lose_corne == m2.can_lose_corne:
+  ## funcao que ordena conforme crescente para o can_lose_corner e decrescente para score_gain 
+  def comper_movos_who_lose_corner(self,m1,m2):
+    if m1.can_lose_corner == m2.can_lose_corner:
       return m2.score_gain - m1.score_gain
-    return m1.can_lose_corne - m2.can_lose_corne
+    return m1.can_lose_corner - m2.can_lose_corner
 
   ## funcao abre a arvore ate o fim usando euristica de maxmizar os pontos
   def maximize_score(self, board, amIMax = True, root = False, depth = 4):
 
     if root:
-      print 'Analizando o tabuleiro'
-      #print board
       for move in RemoveRepeatedMoves(board.valid_moves(self.color)):
-        print move
       self.folha = 0
     
-    # Caso de borda! No folha! Quando o jogo acaba
+    # Caso de borda! Sem folha! Quando o jogo acaba
     if depth == 0 or (len(board.valid_moves(self.color))+len(board.valid_moves(board._opponent(self.color))))==0:
       idx = 0
       if self.color is board.BLACK:
@@ -114,7 +109,6 @@ class Level2QualityPlayer:
           retMove = move
 
       if root: 
-        #print temp_board
         self.folha = 0
 
     if amIMax:
@@ -142,11 +136,11 @@ class MoveQuality:
     self.move = move
     self.color = color
     self.score_gain = 0 # valor inteiro a partir de 1 
-    self.is_corne = False
-    self.can_lose_corne = 0 # valor inteiro de 0 ate 4
-    self.power_to_gain_corne = 0 # valor real entre 0 e 1 
+    self.is_corner = False
+    self.can_lose_corner = 0 # valor inteiro de 0 ate 4
+    self.power_to_gain_corner = 0 # valor real entre 0 e 1 
     self.is_in_small_square = False
-    self.is_on_edgs = False
+    self.is_on_edges = False
 
   def analyze(self, board):
     self.getCorner()
@@ -154,13 +148,13 @@ class MoveQuality:
     self.getMoveCornerLoser(board.get_clone())
     self.getLocation()
 
-  # seta vaiaveis de localisacao do movimento
+  # seta variaveis de localizacao do movimento
   def getLocation(self):
     if 2 < self.move.x < 7 and 2 < self.move.y < 7:
       self.is_in_small_square = True
     elif self.move.x == 1 or self.move.x == 8: 
       if self.move.y == 1 or self.move.y == 8:
-        is_on_edgs = True
+        is_on_edges = True
 
   # seta variaveis relacionadas a quina
   def getMoveCornerLoser(self,board):
@@ -171,17 +165,17 @@ class MoveQuality:
 
     for corner in corners:
       if Move(corner[0],corner[1]) in opponente_moves:
-        self.can_lose_corne += 1
+        self.can_lose_corner += 1
 
-    # o calculo de power_to_gain_corne e: a proporcao de quantas jogadas  
+    # o calculo de power_to_gain_corner e: a proporcao de quantas jogadas  
     # do oponente na proxima rodada me permite ganhar uma quina
     for opponent_move in opponente_moves:
       temp_board = board.get_clone()
       temp_board.play(opponent_move,board._opponent(self.color))
       for corner in corners:
         if Move(corner[0],corner[1]) in temp_board.valid_moves(self.color):
-          self.power_to_gain_corne += 1
-        self.power_to_gain_corne /= 1.0*len(opponente_moves)
+          self.power_to_gain_corner += 1
+        self.power_to_gain_corner /= 1.0*len(opponente_moves)
     
   # seta a quantidade de ponto ganho ao jogar
   def getPointGain(self,board):
@@ -198,7 +192,7 @@ class MoveQuality:
     corners = [[1,1],[1,8], [8,1], [8,8]]
     for corner in corners:
       if Move(corner[0],corner[1]) == self.move:
-        self.is_corne = True
+        self.is_corner = True
         return
   
   def __str__(self):
@@ -206,13 +200,13 @@ class MoveQuality:
     ret += '\n\t'
     ret += 'score_gain = ' + str(self.score_gain)
     ret += '\n\t'
-    ret += 'is_corne = ' + str(self.is_corne)
+    ret += 'is_corner = ' + str(self.is_corner)
     ret += '\n\t'
-    ret += 'can_lose_corne = ' + str(self.can_lose_corne)
+    ret += 'can_lose_corner = ' + str(self.can_lose_corner)
     ret += '\n\t'
-    ret += 'power_to_gain_corne = ' + str(self.power_to_gain_corne)
+    ret += 'power_to_gain_corner = ' + str(self.power_to_gain_corner)
     ret += '\n\t'
     ret += 'is_in_small_square = ' + str(self.is_in_small_square)
     ret += '\n\t'
-    ret += 'is_on_edgs = ' + str(self.is_on_edgs)
+    ret += 'is_on_edges = ' + str(self.is_on_edges)
     return ret
